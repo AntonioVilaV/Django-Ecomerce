0d307a277952d6c0ad77d983b31ea1237ded8b34
Antonio Vila <vila.antoniojose@gmail.com>
2024-02-08 10:34:25 -0400

    add field utils and move context_processors

--- eco/settings/base.py
+++ eco/settings/base.py
@@ -79,7 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'context_processors.categorias_base',
+                'utils.context_processors.categorias_base',
             ],
         },
     },
