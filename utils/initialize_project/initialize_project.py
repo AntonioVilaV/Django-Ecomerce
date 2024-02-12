345cc714bac3433cdd171a3c1408e962e9fd7103
Antonio Vila <vila.antoniojose@gmail.com>
2024-02-12 18:53:32 -0400

    add command for initialize project with data

---
+++ utils/initialize_project/initialize_project.py
@@ -0,0 +1,27 @@
+import os
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.management import call_command
+
+from apps.perfiles.models import datosContacto
+
+
+def initialize_project():
+    user = User.objects.create_superuser("admin", "admin@admin.com", "admin")
+    datosContacto.objects.create(user=user)
+
+    call_command("populate_categories")
+
+    fixture_file = os.path.join(
+        settings.BASE_DIR, "apps", "inventario", "fixtures", "products.json"
+    )
+    call_command("loaddata", fixture_file, verbosity=0)
+
+    call_command("populate_inventory")
+
+    call_command("populate_status_operation")
+
+
+if __name__ == "__main__":
+    initialize_project()
---
+++ utils/sale/status_operation.py
@@ -0,0 +1,6 @@
+STATUS_OPERATION = {
+    1: "Esperando pago",
+    2: "Confirmando pago",
+    3: "Procesando encomienda",
+    4: "Enviado",
+}
